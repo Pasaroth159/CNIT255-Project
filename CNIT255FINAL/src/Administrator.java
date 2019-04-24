da7675209378467346429400aefb673b7faa417d
Pasaroth159 <igrasham12@gmail.com>
2019-04-24 00:24:29 -0400

    Filled out Administrator with adminID

--- CNIT255FINAL/src/Administrator.java
+++ CNIT255FINAL/src/Administrator.java
@@ -1,3 +1,30 @@
+
 public class Administrator extends User {
-    
+
+    private String adminID;
+
+    public Administrator() {
+        super();
+        adminID = "";
+    }
+
+    public Administrator(String adm) {
+        super();
+        adminID = adm;
+    }
+
+    /**
+     * @return the adminID
+     */
+    public String getAdminID() {
+        return adminID;
+    }
+
+    /**
+     * @param adminID the adminID to set
+     */
+    public void setAdminID(String adminID) {
+        this.adminID = adminID;
+    }
+
 }
